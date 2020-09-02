af78defc6980221bc93986376d69fccd718e4fb1
Nick Rioux <nrioux@seas.upenn.edu>
2020-09-01 23:19:47 -0400

    Mutation api

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import Control.Monad.IO.Class (liftIO)
 import Control.Monad.STM (atomically)
 import Control.Monad.Trans.Reader  (ReaderT, ask, runReaderT)
 import Data.Aeson
+import qualified Data.Map.Strict as Map
 import GHC.Generics
 import Network.Wai
 import Network.Wai.Handler.Warp
@@ -18,27 +19,40 @@ import System.Environment
 
 import Lib
 
-data Team = Team { id :: Int, name :: String }
+data Team = Team { key :: Int, name :: String }
   deriving (Eq, Show, Generic)
 instance ToJSON Team
+instance FromJSON Team
 
 data Score = Score { score :: Int, team :: Team }
   deriving (Eq, Show, Generic)
 instance ToJSON Score
+instance FromJSON Score
 
-data Scoreboard = Scoreboard { entries :: [Score] }
-  deriving (Eq, Show, Generic)
-instance ToJSON Scoreboard
+type Scoreboard = Map.Map Int Score
+
+insertScore :: Score -> Scoreboard -> Scoreboard
+insertScore score board =
+  Map.insert (key $ team score) score board
 
 data ServerState = ServerState (TVar Scoreboard)
 type AppM = ReaderT ServerState Handler
 
-type ScoreAPI = "score" :> Get '[JSON] Scoreboard
+type GetScore = Get '[JSON] Scoreboard
+type AddScore = ReqBody '[JSON] Score :> Post '[JSON] ()
+
+type ScoreAPI = "score" :> (GetScore :<|> AddScore)
 scoreServer :: ServerT ScoreAPI AppM
-scoreServer = do
-  ServerState boardptr <- ask
-  board <- liftIO $ atomically $ readTVar boardptr
-  return board
+scoreServer = getScore :<|> addScore
+  where
+    getScore = do ServerState boardptr <- ask
+                  liftIO $ atomically $ readTVar boardptr
+    addScore score = do ServerState boardptr <- ask
+                        liftIO $ updateBoard score boardptr
+    updateBoard score boardptr = atomically $ do
+      board <- readTVar boardptr
+      let board' = insertScore score board
+      writeTVar boardptr board'
 
 type StaticAPI = Raw
 
@@ -66,7 +80,7 @@ main :: IO ()
 main = do
   cd <- getCurrentDirectory
   portStr <- getEnv "PORT"
-  board <- atomically $ newTVar $ Scoreboard { entries = [] }
+  board <- atomically $ newTVar $ Map.singleton 0 Score { score = 10, team = Team { key = 1, name = "Green Team" } }
   let port = read portStr
   putStrLn $ "CD: " ++ cd
   putStrLn $ "Running server: http://localhost:" ++ (show port)
